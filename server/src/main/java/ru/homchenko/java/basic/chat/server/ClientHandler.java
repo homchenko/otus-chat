5ad1133b0277f4f0d882becd2f59c8b869fe72ea
d.homchenko <posharkow@mail.ru>
2024-04-02 14:58:31 +0300

    classes2

--- server/src/main/java/ru/homchenko/java/basic/chat/server/ClientHandler.java
+++ server/src/main/java/ru/homchenko/java/basic/chat/server/ClientHandler.java
@@ -10,17 +10,10 @@ public class ClientHandler {
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
+    private String nickname;
 
-    private String username;
-    private static int usersCounter = 0;
-
-    private void generateUserName() {
-        usersCounter++;
-        this.username = "user" + usersCounter;
-    }
-
-    public String getUsername() {
-        return username;
+    public String getNickname() {
+        return nickname;
     }
 
     public ClientHandler(Server server, Socket socket) throws IOException {
@@ -28,36 +21,92 @@ public class ClientHandler {
         this.socket = socket;
         this.in = new DataInputStream(socket.getInputStream());
         this.out = new DataOutputStream(socket.getOutputStream());
-        this.generateUserName();
+
         new Thread(() -> {
             try {
-                //System.out.println("Подключился новый клиент");
-                String name = in.readUTF();
-                if (name != null)
-                    this.username = name;
-                System.out.println("Подключился новый клиент: " + this.username);
-
-                while (true) {
-                    String msg = in.readUTF();
-
-                    if (msg.startsWith("/w ")) {
-                        server.sendMessageSelectedUser(msg);
-                    } else {
-                        server.broadcastMessage(username + ": " + msg);
-                    }
-
-                    if (msg.startsWith("/exit")) {
-                        disconnect();
-                        break;
-                    }
+                System.out.println("Подключился новый клиент");
+                if (tryToAuthenticate()) {
+                    communicate();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
                 disconnect();
             }
-
         }).start();
+    }
+
+    private void communicate() throws IOException {
+        while (true) {
+            String msg = in.readUTF();
+            if (msg.startsWith("/")) {
+                if (msg.startsWith("/exit")) {
+                    break;
+                }
+                if (msg.startsWith("/w ")) {
+                    server.sendMessageSelectedUser(msg);
+                }
+                continue;
+            }
+            server.broadcastMessage(nickname + ": " + msg);
+        }
+    }
+
+    private boolean tryToAuthenticate() throws IOException {
+        while (true) {
+            String msg = in.readUTF();
+            if (msg.startsWith("/auth ")) {
+                String[] tokens = msg.split(" ");
+                if (tokens.length != 3) {
+                    sendMessage("Некорректный формат запроса");
+                    continue;
+                }
+                String login = tokens[1];
+                String password = tokens[2];
+                String nickname = server.getAuthenticationService().getNicknameByLoginAndPassword(login, password);
+                if (nickname == null) {
+                    sendMessage("Неправильный логин/пароль");
+                    continue;
+                }
+                if (server.isNicknameBusy(nickname)) {
+                    sendMessage("Указанная учетная запись уже занята. Попробуйте зайти позднее");
+                    continue;
+                }
+                this.nickname = nickname;
+                server.subscribe(this);
+                sendMessage(nickname + ", добро пожаловать в чат!");
+                return true;
+            } else if (msg.startsWith("/register ")) {
+                String[] tokens = msg.split(" ");
+                if (tokens.length != 4) {
+                    sendMessage("Некорректный формат запроса");
+                    continue;
+                }
+                String login = tokens[1];
+                String password = tokens[2];
+                String nickname = tokens[3];
+                if (server.getAuthenticationService().isLoginAlreadyExist(login)) {
+                    sendMessage("Указанный логин уже занят");
+                    continue;
+                }
+                if (server.getAuthenticationService().isNicknameAlreadyExist(nickname)) {
+                    sendMessage("Указанный никнейм уже занят");
+                    continue;
+                }
+                if (!server.getAuthenticationService().register(login, password, nickname)) {
+                    sendMessage("Не удалось пройти регистрацию");
+                    continue;
+                }
+                this.nickname = nickname;
+                server.subscribe(this);
+                sendMessage("Вы успешно зарегистрировались! " + nickname + ", добро пожаловать в чат!");
+                return true;
+            } else if (msg.equals("/exit")) {
+                return false;
+            } else {
+                sendMessage("Вам необходимо авторизоваться");
+            }
+        }
     }
 
     public void sendMessage(String msg) {
--- server/src/main/java/ru/homchenko/java/basic/chat/server/Server.java
+++ server/src/main/java/ru/homchenko/java/basic/chat/server/Server.java
@@ -12,18 +12,29 @@ public class Server {
     private int port;
     private List<ClientHandler> clients;
 
+    private AuthenticationService authenticationService;
+
+    public AuthenticationService getAuthenticationService() {
+        return authenticationService;
+    }
+
     public Server(int port) {
         this.port = port;
         this.clients = new ArrayList<>();
     }
 
     public void start() {
-        try (ServerSocket serverSocket = new ServerSocket(this.port)) {
-            System.out.printf("Сервер запущен на порте: %d, ожидаем подключения клиентов\n", this.port);
-
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            this.authenticationService = new InMemoryAuthenticationService();
+            System.out.println("Сервис аутентификации запущен: " + authenticationService.getClass().getSimpleName());
+            System.out.printf("Сервер запущен на порту: %d, ожидаем подключения клиентов\n", port);
             while (true) {
-                Socket socket = serverSocket.accept();
-                this.subscribe(new ClientHandler(this, socket));
+                try {
+                    Socket socket = serverSocket.accept();
+                    new ClientHandler(this, socket);
+                } catch (Exception e) {
+                    System.out.println("Возникла ошибка при обработке подключившегося клиента");
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -45,21 +56,28 @@ public class Server {
     }
 
     public synchronized void sendMessageSelectedUser(String msg) {
-        int spaceIdx1 = msg.indexOf(" ");
-        String subStr = msg.substring(spaceIdx1 + 1);
-        int spaceIdx2 = subStr.indexOf(" ");
-        String userName = subStr.substring(0, spaceIdx2);
+        String[] tokens = msg.split(" ");
+        String nickname = tokens[1];
 
         Map<String, ClientHandler> usersMap = new HashMap<>();
         for (ClientHandler client : clients) {
-            usersMap.put(client.getUsername(), client);
+            usersMap.put(client.getNickname(), client);
         }
 
         for (Map.Entry<String, ClientHandler> entry : usersMap.entrySet()) {
-            if (entry.getKey().equals(userName)) {
+            if (entry.getKey().equals(nickname)) {
                 entry.getValue().sendMessage(msg);
                 break;
             }
         }
+    }
+
+    public synchronized boolean isNicknameBusy(String nickname) {
+        for (ClientHandler c : clients) {
+            if (c.getNickname().equals(nickname)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
